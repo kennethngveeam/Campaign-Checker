1173acf3767ec406f96b31599279524a901e30d7
kennethngveeam <167235408+kennethngveeam@users.noreply.github.com>
2024-05-27 15:59:44 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -12,13 +12,23 @@ app = Flask(__name__)
 def find_non_matching_emails(listupload_path, sfdc_path):
     try:
         listupload = pd.read_excel(listupload_path)
-        sfdc = pd.read_excel(sfdc_path, skiprows=10)
+        
+        # Read SFDC file without specifying header to automatically detect it
+        sfdc = pd.read_excel(sfdc_path, header=None)
+        sfdc= sfdc.astype(str)
+        # Find the correct header row (assuming it's the row containing 'Member Email')
+        header_row = sfdc.apply(lambda row: row.str.contains('Member Email', case=False, na=False)).any(axis=1).idxmax()
+        
+        # Read the SFDC file again with the correct header row
+        sfdc = pd.read_excel(sfdc_path, header=header_row)
         sfdc = sfdc.dropna(subset=['Member Email'])
+        
         emails_in_listupload = listupload['Email Address'].str.lower().dropna().unique()
         emails_in_sfdc = sfdc['Member Email'].str.lower().dropna().unique()
         
         non_matching_details = listupload[~listupload['Email Address'].str.lower().isin(emails_in_sfdc)]
         non_matching_details = non_matching_details[['First Name', 'Last Name', 'Email Address', 'Company Name']]
+        
         # Remove rows where all specified columns are NaN
         non_matching_details = non_matching_details.dropna(how='all', subset=['Email Address'])
         return non_matching_details.to_dict(orient='records')
